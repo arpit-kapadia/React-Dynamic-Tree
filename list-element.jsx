e71b6ef640094ebe724931c23604c053d9f6386b
Arpit <kapadiaarpit@gmail.com>
2018-01-08 09:44:42 +0530

    WIP: Addd vertical lines
    It's in testing phase. 
    # Added vertical lines
    # Modified div styles
    # swap containers with inline-blocks and removed margins
    # added background colors to every div for testing purpose

--- list-element.jsx
+++ list-element.jsx
@@ -12,6 +12,7 @@ class Li extends Component {
     this.state = {
       buttonHover: false,
       linkHover: false,
+      subDivHeight: 0,
     }
   }
 
@@ -28,25 +29,26 @@ class Li extends Component {
       borderRadius: '50%',
       background: 'rgba(0,0,0,0)',
       border: 'none',
-      width: '30px',
-      height: '30px',
-      padding: '2px',
-      fontSize: '28px',
+      // width: '30px',
+      // height: '30px',
+      // paddingBottom: '2px',
+      fontSize: '16px',
       fontWeight: '600',
       color: 'rgba(0,0,0,0.17)'
     };
     let linkStyle = {
-      height: '20px',
-      width: '20px',
+      // height: '20px',
+      // width: '20px',
       padding: '2px',
       borderRadius: '50%',
-      marginRight: '8px',
+      // marginRight: '8px',
       color: '#414141',
+      fontSize: '16px',
       background: '#414141',
       border: '6px solid #DDDDDD'
     }
     let { item } = this.props;
-    let key = 'title = ' + item.title + '-' + item._id;
+    let key = `key-${item._id}`;
 
     if (this.state.buttonHover) {
       buttonStyle.color = '#313131'
@@ -60,55 +62,102 @@ class Li extends Component {
       linkStyle.border = '6px solid white';
     }
 
+    const subDivId = `id-sublist-${item._id}`;
+    const subDivElement = document.getElementById(subDivId);
+    let subDivHeight = (subDivElement && subDivElement.clientHeight) || 0;
+    if (subDivHeight) subDivHeight -= 10;
+
     return (
       <div
-        className={'row container-fluid'}
         key={key}
+        id={key}
         style={{
-          minHeight: '35px',
-          paddingRight: '30px',
-          marginLeft: '30px',
-          minWidth: '400px!important'
+          // minHeight: '30px',
+          // paddingRight: '30px',
+          // marginLeft: '30px',
+          // padding: '1px',
+          background: 'rgba(0,0,0,0.08)'
+          // minWidth: '400px!important'
         }}
       >
-        <div className={''} style={{display: 'inline-block'}}>
-          <button
-            onClick={() => this.props.toggleExpansion(item.title)}                       
-            style={buttonStyle}
-            onMouseEnter={() => this.toggleHover('buttonHover')}
-            onMouseLeave={() => this.toggleHover('buttonHover')}
+        <div className={'inline-block'} style={{verticalAlign: 'top', textAlign: 'right', height: '100%', background: 'rgba(225,220,100,0.8)'}}>
+          <div
+            style={{
+              display: 'inline-block',
+              verticalAlign: 'top',
+              background: 'rgba(255,0,0,0.1)',
+              // margin: '0 auto',
+              // paddingBottom: '2px',
+              // paddingLeft: '1px',
+            }}
           >
-            { item.expanded ? '-' : '+' }
-          </button>
-        </div>
+            <button
+              onClick={() => this.props.toggleExpansion(item.title)}                       
+              style={buttonStyle}
+              onMouseEnter={() => this.toggleHover('buttonHover')}
+              onMouseLeave={() => this.toggleHover('buttonHover')}
+            >
+              { item.expanded ? '-' : '+' }
+            </button>
+          </div>
 
-        <div className={''} style={{display: 'inline-block', paddingBottom: '0px!important', paddingBottom: '3px', paddingLeft: '2px', marginRight: '2px'}}>
-          <button
-            onClick={() => this.navigate(item._id)}
-            style={linkStyle}
-            onMouseEnter={() => this.toggleHover('linkHover')}
-            onMouseLeave={() => this.toggleHover('linkHover')}
+          <div
+            style={{
+              display: 'inline-block',
+              background: 'rgba(0,255,0,0.1)',
+              height: '100%',
+              textAlign: 'center',
+              // paddingBottom: '0px!important',
+              // paddingBottom: '3px',
+              // paddingLeft: '2px',
+            }}
           >
-          </button>
+            <div>
+              <button
+                onClick={() => this.navigate(item._id)}
+                style={linkStyle}
+                onMouseEnter={() => this.toggleHover('linkHover')}
+                onMouseLeave={() => this.toggleHover('linkHover')}
+              >
+              </button>
+            </div>
+            <div className={'outer'} style={{height: subDivHeight}}>
+              <div className={'inner'}></div>
+            </div>
+            { /*
+              item.expanded
+              ? <div className={'outer'} style={{height: subDivHeight}}>
+                  <div className={'inner'}></div>
+                </div>
+              : ''
+            */}
+          </div>
         </div>
 
-        <div
-          id={`id-${item._id}`}
-          tabIndex={this.props.tabIndex}
-          className={'title-div'}
-          style={{display: 'inline-block', paddingTop: '3px', minWidth: '300px', minHeight: '100%',}}
-          key={item.title + item._id + item._id}
-          contentEditable={true}
-          suppressContentEditableWarning
-          onKeyDown={(e) => this.props.specialKeyPressed(e, item)}
-          onKeyUp={() => this.props.specialKeyReleased()}
-          onBlur={(e) => this.props.modifyTitle(e, item)}
-        >
-          { item.title }
-        </div>
+        <div className={'inline-block'} style={{textAlign: 'left'}}>
+          <div
+            id={`id-${item._id}`}
+            tabIndex={this.props.tabIndex}
+            className={'title-div'}
+            style={{
+              background: 'rgba(0,0,255,0.1)',
+              minWidth: '300px',
+              // minHeight: '30px'
+              height: 'auto',
+            }}
+            key={item.title + item._id + item._id}
+            contentEditable={true}
+            suppressContentEditableWarning
+            onKeyDown={(e) => this.props.specialKeyPressed(e, item)}
+            onKeyUp={() => this.props.specialKeyReleased()}
+            onBlur={(e) => this.props.modifyTitle(e, item)}
+          >
+            { item.title }
+          </div>
 
-        <div>
-          { item.expanded ? this.props.createTree(item._id, this.props.data) : '' }
+          <div id={subDivId}>
+            { item.expanded ? this.props.createTree(item._id, this.props.data) : '' }
+          </div>
         </div>
       </div>
     );
